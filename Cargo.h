e866778a99eb3fc2a6ba4e4a272d65f2b20c6897
Mahmoud Sobhy <mahsobhy3@gmail.com>
2022-04-01 01:06:27 +0200

    Final

--- Cargo.h
+++ Cargo.h
@@ -1,27 +1,47 @@
 #pragma once
 #include <iostream>
+#include"Def.h"
 #include<string>
 using namespace std;
-enum Cargo_Type {};
+
 class Cargo
 {
 private:
 	string Preparation_Time; //Time(day:hour) at which the cargo is ready to be loaded
-	 
 	float Load_Unload_Time;
-	Cargo_Type Type;
+	CARGO_TYPE Type;
 	double Delivery_Distance; //Km
 	double Cost;  
 
 	public:
 
-		Cargo(string, float, Cargo_Type, double, double);
-		double GetDistance();
-		double GetCost();
-		float GetLU_Time();
-		Cargo_Type GetType();
-		string GetPrepTime();
-		void PromoteToVip(double);
+		Cargo(string PT, float LT, CARGO_TYPE T, double DD, double C) {
+			Preparation_Time = PT;
+			Load_Unload_Time = LT;
+			Delivery_Distance = DD;
+			Type = T;
+			Cost = C;
+		}
+		double GetDistance() {
+			return Delivery_Distance;
+		}
+		double GetCost() {
+			return Cost;
+		}
+		float GetLU_Time() {
+			return Load_Unload_Time;
+		}
+		CARGO_TYPE GetType() {
+			return Type;
+
+		}
+		string GetPrepTime() {
+			return Preparation_Time;
+		}
+		void PromoteToVip(double ExtraMoney) {
+			Type = CARGO_TYPE:: VIP;
+			Cost += ExtraMoney;
+		}
 	
 
 
--- Truck.h
+++ Truck.h
@@ -2,24 +2,44 @@
 #include <iostream>
 #include <iostream>
 #include<string>
+#include"Def.h"
 using namespace std;
-enum Truck_Type {};
+
 class Truck
 {
 private:
-	Truck_Type Type;
+	TRUCK_TYPE Type;
 	int Truck_Capacity; //# of cargos
 	float Maintenance_Time; //hours
 	int J; //Number of delivery journeys before maintenance
 	float Speed; //Km/h
 	float Delivery_Interval; //Time to deliver all cargos & comback, Calculated
 public:
-	Truck(Truck_Type, int, float, int, float, float);
-	Truck_Type GetType();
-	int GetCapacity();
-	int GetJ();
-	float GetMaintenanceTime();
-	float GetSpeed();
-	float GetDeliveryInterval();
+	Truck(TRUCK_TYPE T, int TC, float MT, int j, float S, float DI) {
+		Type = T;
+		Truck_Capacity = TC;
+		Maintenance_Time = MT;
+		J = j;
+		Speed = S;
+		Delivery_Interval = DI;
+	}
+	TRUCK_TYPE GetType() {
+		return Type;
+	}
+	int GetCapacity() {
+		return Truck_Capacity;
+	}
+	int GetJ() {
+		return J;
+	}
+	float GetMaintenanceTime() {
+		return Maintenance_Time;
+	}
+	float GetSpeed() {
+		return Speed;
+	}
+	float GetDeliveryInterval() {
+		return Delivery_Interval;
+	}
 };
 
