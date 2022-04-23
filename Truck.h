48182f520a78c3cc020dce9ad499b9adb1c4ea81
Mahmoud Sobhy <mahsobhy3@gmail.com>
2022-04-23 16:02:55 +0200

    ..

--- Truck.h
+++ Truck.h
@@ -9,15 +9,14 @@ private:
 	TRUCK_TYPE Type;
 	int Truck_Capacity; //# of cargos
 	float Maintenance_Time; //hours
-	int J; //Number of delivery journeys before maintenance
 	float Speed; //Km/h
 	float Delivery_Interval; //Time to deliver all cargos & comeback, Calculated
 public:
-	Truck(TRUCK_TYPE T, int TC, float MT, int j, float S) {
+	Truck(TRUCK_TYPE T, int TC, float MT, float S) {
 		Type = T;
 		Truck_Capacity = TC;
 		Maintenance_Time = MT;
-		J = j;
+		
 		Speed = S;
 	}
 	TRUCK_TYPE GetType() {
@@ -26,9 +25,7 @@ public:
 	int GetCapacity() {
 		return Truck_Capacity;
 	}
-	int GetJ() {
-		return J;
-	}
+	
 	float GetMaintenanceTime() {
 		return Maintenance_Time;
 	}
